5d7f6df0e69aeeb19d4f11eb9591a351b5b59bfd
Robert Garner <rgarner011235@gmail.com>
2025-10-06 15:23:28 -0600

    Completed coding modes

---
+++ UtilitiesTests/InsTKStringUtilitiesTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using Utilities;
+
+namespace UtilitiesTests
+{
+    [TestClass]
+    public sealed class InsTKStringUtilitiesTests
+    {
+        [TestMethod]
+        public void ShouldFindOneYInMysterious()
+        {
+            var stringToCheck = "mysterious";  
+            var stringToFind = "y";
+            var expectedResult = 1;
+            var classUnderTest = new InsTKStringUtilities();
+            int actualResult = classUnderTest.CountOccurances(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ShouldFindTwoSInMysterious()
+        {
+            var stringToCheck = "mysterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+            var classUnderTest = new InsTKStringUtilities();
+            var actualResult = classUnderTest.CountOccurances(stringToCheck,stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SearchShouldBeCaseInsensitive()
+        {
+            var stringToCheck = "mySterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+            var classUnderTest = new InsTKStringUtilities();
+            var actualResult = classUnderTest.CountOccurances(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SearchShouldBeCaseInsensitiveGivenUppercaseStringToFind()
+        {
+            var stringToCheck = "mysterious";
+            var stringToFind = "S";
+            var expectedResult = 2;
+            var classUnderTest = new InsTKStringUtilities();
+            var actualResult = classUnderTest.CountOccurances(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ShoulhBeAbleToHandleNulls()
+        {
+            var stringToCheck = (string?)null;
+            var stringToFind = "s";
+            var expectedResult = -1;
+            var classUnderTest = new InsTKStringUtilities();
+            var actualResult = classUnderTest.CountOccurances(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void NotFoundShouldReturnMinus1()
+        {
+            var stringToCheck ="mysterious";
+            var stringToFind = "z";
+            var expectedResult = 0;
+            var classUnderTest = new InsTKStringUtilities();
+            var actualResult = classUnderTest.CountOccurances(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
